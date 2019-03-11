5b1e40df37e9f6b06cd2065d57e950ef34df9a91
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-03-11 13:34:06 -0400

    ini UnityProject again

---
+++ Codelab1-2019-Class5/Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreManager : MonoBehaviour
+{
+    private const string HIGH_SCORE_FILE = "\myHighScore.txt"; //use this to set the name of hgihscore record file
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+             
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
