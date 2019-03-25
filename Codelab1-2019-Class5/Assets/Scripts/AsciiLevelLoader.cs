808771fd6f6e94b1bb8bfb2754613f1423ab83ed
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-03-25 18:11:04 -0400

    check In for Homework

---
+++ Codelab1-2019-Class5/Assets/Scripts/AsciiLevelLoader.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AsciiLevelLoader : MonoBehaviour
+{
+    private const string LEVEL0 = "/level0.txt";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string filePath = Application.dataPath + LEVEL0;
+
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath,"X");
+        }
+
+        string[] inputLines = File.ReadAllLines(filePath);
+        
+        for (int y = 0; y < inputLines.Length; y++)
+        {
+            string line = inputLines[y];
+            for (int x = 0; x < line.Length; x++)
+            {
+                if (line[x] == 'x') 
+                {
+                    GameObject newWall = Instantiate(Resources.Load("Prefabs/Wall")) as GameObject;
+                    newWall.transform.position = new Vector3(x-line.Length/2f,inputLines.Length/2f-y);
+                }
+                
+                if (line[x] == 'P') 
+                {
+                    GameObject newWall = Instantiate(Resources.Load("Prefabs/Player")) as GameObject;
+                    newWall.transform.position = new Vector3(x-line.Length/2f,inputLines.Length/2f-y);
+                }
+                
+                if (line[x] == 'G') 
+                {
+                    GameObject newWall = Instantiate(Resources.Load("Prefabs/Gold")) as GameObject;
+                    newWall.transform.position = new Vector3(x-line.Length/2f,inputLines.Length/2f-y);
+                }
+                
+                if (line[x] == 'T') 
+                {
+                    GameObject newWall = Instantiate(Resources.Load("Prefabs/Trap")) as GameObject;
+                    newWall.transform.position = new Vector3(x-line.Length/2f,inputLines.Length/2f-y);
+                }
+            }      
+        }           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Codelab1-2019-Class5/Assets/Scripts/HighScoreManager.cs
+++ Codelab1-2019-Class5/Assets/Scripts/HighScoreManager.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class HighScoreManager : MonoBehaviour
 {
-    private const string HIGH_SCORE_FILE = "\myHighScore.txt"; //use this to set the name of hgihscore record file
-    
-    
+    private const string HIGH_SCORE_FILE = "/myHighScore.txt"; //use this to set the name of hgihscore record file
+
+    public List<string> hsNames;
+    public List<int> hsScores;
     
     // Start is called before the first frame update
     void Start()
     {
-       
-             
+        string filePath = Application.dataPath + HIGH_SCORE_FILE;
+
+        if (!File.Exists(filePath)) //if the file does not exist
+        {
+            //create the file
+            string output = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                output += "Matt:" + (10-i) + '\n';
+            }
+            
+            Debug.Log(output);
+            File.WriteAllText(filePath,output);
+        }
+        
+        //do this if wanna make it private
+        //hsNames = new List<string>();
+        //hsScores = new List<int>();
+        
+        //if file exist, add data to the game
+        string[] inputLines = File.ReadAllLines(filePath);
+        for (int i = 0; i < inputLines.Length; i++)
+        {
+            string line = inputLines[i]; //ex: "Matt:10"
+            string[] splitLine = line.Split(':'); //ex: "Matt" | "10"
+            string name = splitLine[0]; //ex: "Matt"
+            int score = Int32.Parse(splitLine[1]); //ex: "10" > 10
+            
+            hsNames.Add(name);
+            hsScores.Add(score);
+        }
+
     }
 
     // Update is called once per frame
